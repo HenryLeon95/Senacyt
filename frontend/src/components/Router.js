359da4320fcd63c981aee7a3d20196d9bf446991
Henry León <henriscoh1995@gmail.com>
2021-09-18 13:10:01 -0600

    Primer commit

---
+++ frontend/src/components/Router.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import Alumnos from "./Alumnos";
+
+export default class Router extends Component {
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <Menu></Menu>
+                    <Switch>
+                        {/* <Route exact path="/" component={Alumnos}></Route> */}
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        );
+    }
+}
